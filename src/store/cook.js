1c024fb21ed64a01f058ba700f0d793ae0eafc31
jinzhuming <jinzhuming0308@gmail.com>
2018-07-31 00:16:54 +0800

    更改注释

--- src/store/cook.js
+++ src/store/cook.js
@@ -29,28 +29,31 @@ const getters = {
 
 const mutations = {
   setCookList(state, { list, categoryId }) {
-    // 获取到的数据插入最后
+    // 获取到的数据搭配当前 id 组合后插入数组
     state.cookList.push({
       categoryId,
       list,
     })
   },
   setActiveCategoryId(state, categoryId) {
-    // 获取到的数据插入最后
     state.activeCategoryId = categoryId
   },
 }
 
 const actions = {
   async getCookList({ commit, state }, id) {
+    // 设置当前选择 id
     commit('setActiveCategoryId', id)
-    // 判断数组里有没有获取的 category 数据
+
+    // 判断数组里有没有当前选择的 category 数据
     const index = _.findIndex(state.cookList, ['categoryId', id])
     if (index === -1) {
       try {
         const res = await wx.$http({
           url: `/cook/${id}`,
         })
+
+        // 存放获取到的数据以及当前的 id
         commit('setCookList', { list: res.data, categoryId: id })
       } catch (err) {
         wx.$showToast({
