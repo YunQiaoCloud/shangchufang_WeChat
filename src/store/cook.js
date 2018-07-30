10da4a836c3c5707ce31b998bbf0a3b1aa8133f7
jinzhuming <jinzhuming0308@gmail.com>
2018-07-30 23:57:48 +0800

    修改 vuex 获取 cooklist 逻辑；

--- src/store/cook.js
+++ src/store/cook.js
@@ -1,33 +1,54 @@
+import _ from 'lodash'
+
 const state = {
-  cookList: {
-    categoresId: '',
-    list: {},
-  },
+  /*
+    cookList: {
+      categoryId: '',
+      list: {},
+    },
+  */
+  activeCategoryId: '', // 当前选择的是哪一个 tab
+  cookList: [],
 }
 
 const getters = {
   cookList(state) {
-    return state.cookList.list
+    // 找到有没有当前选择的菜单对应的数据
+    const index = _.findIndex(state.cookList, ['categoryId', state.activeCategoryId])
+
+    // 有就返回数据，否则返回空数组
+    if (index !== -1) {
+      return state.cookList[index].list
+    }
+    return []
   },
 }
 
 const mutations = {
-  setCookList(state, cookList) {
-    state.cookList.list = cookList
-    console.log(state.cookList, 'cookList')
+  setCookList(state, { list, categoryId }) {
+    // 获取到的数据插入最后
+    state.cookList.push({
+      categoryId,
+      list,
+    })
+  },
+  setActiveCategoryId(state, categoryId) {
+    // 获取到的数据插入最后
+    state.activeCategoryId = categoryId
   },
 }
 
 const actions = {
-  async getCookList({ commit, state }, index) {
-    if (state.cookList.categoresId !== index) {
+  async getCookList({ commit, state }, id) {
+    commit('setActiveCategoryId', id)
+    // 判断数组里有没有获取的 category 数据
+    const index = _.findIndex(state.cookList, ['categoryId', id])
+    if (index === -1) {
       try {
         const res = await wx.$http({
-          url: `/cook/${index}`,
+          url: `/cook/${id}`,
         })
-        const categoresId = index
-        state.cookList.categoresId = categoresId
-        commit('setCookList', res.data)
+        commit('setCookList', { list: res.data, categoryId: id })
       } catch (err) {
         wx.$showToast({
           title: '获取列表失败',
